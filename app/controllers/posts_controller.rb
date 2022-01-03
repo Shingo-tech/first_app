ea0ef6f0b9912cdcd0ea52d352c0e60d6992a938
EC2 Default User <ec2-user@ip-172-31-46-38.ap-northeast-1.compute.internal>
2022-01-03 02:38:00 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+# コントローラーファイル
+class PostsController < ApplicationController
+    
+    def index  # indexアクションを定義した
+        # @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+        # @post = Post.find(1) #１番目のレコードを@postに代入
+        @posts = Post.all #全てのレコードを@postsに代入
+    end
+    
+    def new
+    end
+    
+    def create
+        Post.create(content: params[:memo])
+    end
+end
